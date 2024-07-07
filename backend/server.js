1457810d7b096f8cc1308a5d4b63ebe68296888d
HarshN187 <hnirmal913@gmail.com>
2024-07-07 15:53:16 +0530

    notifiaction routes and controller compelete

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
-import authRoutes from "./routes/auth.routes.js";
+import authRoutes from "./routes/auth.route.js";
 import connectMongoDB from "./db/connectMongoDB.js";
 import cookieParser from "cookie-parser";
-import userRoutes from "./routes/user.routes.js";
+import userRoutes from "./routes/user.route.js";
 import { v2 as cloudinary } from "cloudinary";
-import postRoutes from "./routes/post.routes.js";
+import postRoutes from "./routes/post.route.js";
+import notificationRoutes from "./routes/notification.route.js";
 
 dotenv.config();
 
@@ -23,6 +24,7 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
+app.use("/api/notifications", notificationRoutes);
 
 const port = process.env.PORT || 8000;
 
